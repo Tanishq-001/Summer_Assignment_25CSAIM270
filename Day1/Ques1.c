ad3788601e8ef3e8afa3eb37e785e5346fb3be50
Tanishq chaudhary <tcshts51@gmail.com>
2026-06-17 23:55:42 +0530

    Question 2 update

--- Day1/Ques1.c
+++ Day1/Ques1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    int n, sum = 0;
+int main()
+{
+    int n,i, sum = 0;
+
+
     printf("Enter a positive integer N: ");
     scanf("%d", &n);
 
-    for(int i = 1; i <= n; i++) {
+    for(i = 1; i <= n; i++)
+    {
         sum += i;
     }
 
     printf("Sum of first %d natural numbers = %d\n", n, sum);
+
     return 0;
 }
